7737960aa0988276b47ca1c6371ddb33a3b30ad0
ernestkoko <ernesteferetin@yahoo.com>
2020-04-10 11:59:10 +0100

    Created a controller and a model for the the View and returned a message body

--- src/main/java/com/example/demo/controller/MessageController.java
+++ src/main/java/com/example/demo/controller/MessageController.java
@@ -1,4 +1,18 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Message;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class MessageController {
+
+    //url mapping
+    @ResponseBody  // this tells spring we are returning Message object and spring converts the message to JSON format
+    @GetMapping("/message")  //this maps the url ./message to this method, send()
+    Message send(){
+        return new Message("first message");
+    }
+
 }

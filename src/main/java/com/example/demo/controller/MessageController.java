2ba4fb717effef23d6421239c033c7df4acb09a1
ernestkoko <ernesteferetin@yahoo.com>
2020-04-10 12:14:42 +0100

    I changed @Controller to @RestController so I could remove the @ResponseBody from the method

--- src/main/java/com/example/demo/controller/MessageController.java
+++ src/main/java/com/example/demo/controller/MessageController.java
@@ -1,18 +1,28 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.Message;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+//when you make @Controller a @RestController.The @ResponseBody on the send() method can be removed
+
+@RestController
 public class MessageController {
 
     //url mapping
-    @ResponseBody  // this tells spring we are returning Message object and spring converts the message to JSON format
+   // @ResponseBody  // this tells spring we are returning Message object and spring converts the message to JSON format
     @GetMapping("/message")  //this maps the url ./message to this method, send()
     Message send(){
         return new Message("first message");
+    }
+
+    @PostMapping("/message")
+    Message echo(@RequestBody Message message){
+        return  message;
     }
 
 }
